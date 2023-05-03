593f428a0a9d8949c07306726533c71bd1f7e2e7
hcutter <110829589+hcutter@users.noreply.github.com>
2023-05-02 22:28:20 -0400

    added Javascript to update the date and time
    Using moment, the date and time of San Francisco and Dublin are updated every second.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+function updateTime() {
+    let sanFranciscoElement = document.querySelector("#san-francisco");
+    let sanFranciscoDateElement = sanFranciscoElement.querySelector(".date");
+    let sanFranciscoTimeElement = sanFranciscoElement.querySelector(".time");
+    let sanFranciscoTime = moment().tz("America/San_Francisco");
+    
+    sanFranciscoDateElement.innerHTML = sanFranciscoTime.format("mmmm Do, YYYY");
+    sanFranciscoTimeElement.innerHTML = sanFranciscoTime.format("h:mm:ss [<small>]A[</small>]");
+    
+    let dublinElement = document.querySelector("#dublin");
+    let dublinDateElement = dublinElement.querySelector(".date");
+    let dublinTimeElement = dublinElement.querySelector(".time");
+    let dublinTime = moment().tz("Europe/Dublin");
+    
+    dublinDateElement.innerHTML = dublinTime.format("mmmm Do, YYYY");
+    dublinTimeElement.innerHTML = dublinTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
